47666f7d3c970b73d7d333d76375ee752e592e5e
Devesh Chandra <deveshru2712@gmail.com>
2025-09-02 23:05:13 +0530

    fix: fixing the shadow issue with no-icon button

--- components/Keys.tsx
+++ components/Keys.tsx
@@ -16,7 +16,7 @@ export interface VariantClass {
 const ClassBasedOnVariant: VariantClass = {
   circle:
     "h-7 w-7 rounded-full border border-slate-50/20 group-hover:border-slate-50/10 flex items-center justify-center",
-  noIcon: "h-full w-full flex items-end px-2 pb-2",
+  noIcon: "flex items-end px-2 pb-2",
   special:
     "bg-gradient-to-b from-slate-600 to-slate-800 border border-slate-400/30 flex flex-col items-center justify-center",
 };
@@ -41,7 +41,7 @@ const Keys = ({ icon, text, className, variant = "default" }: KeysProps) => {
     return (
       <div className="group relative mx-1.5 my-1 h-12 w-fit min-w-[75px] rounded-md p-[0.5px] text-white shadow-[1px_4px_6px_rgb(225,225,225,0.6)] transition-all duration-300 hover:shadow-[1px_4px_6px_rgb(0,0,0,0.1)]">
         <div
-          className={`h-full rounded-md text-[10px] shadow-[1px_4px_6px_rgb(225,225,225,0.6)] hover:scale-95 ${variantClasses} `}
+          className={`absolute inset-0.5 z-10 rounded-md text-[10px] shadow-[1px_4px_6px_rgb(225,225,225,0.6)] hover:scale-95 ${variantClasses} `}
         >
           <div className={`${className} flex w-full`}>{text}</div>
         </div>
